b807ae98a4ceafdb21530f6453e2ed7b789e1881
Silver.btc <silvernight.luther@gmail.com>
2024-10-21 19:21:39 +0200

    tree base functions

--- src/main.c
+++ src/main.c
@@ -1,5 +1,90 @@
 #include <stdio.h>
 #include "map.h"
+#include "loc.h"
+
+
+typedef struct s_node
+{
+    struct s_node *children[9];
+    int value; 
+} t_node;
+
+typedef struct s_tree
+{
+    t_node *root;
+} t_tree;
+
+t_tree createEmptyTree(){
+    t_tree temp;
+    temp.root = NULL;
+    return temp;
+}
+
+t_node *createNode(int val){
+    t_node *node = malloc(sizeof(t_node));
+    if (node == NULL) {
+        fprintf(stderr, "Erreur d'allocation de mémoire\n");
+        exit(1);
+    }
+    node->value = val;
+    for (int i = 0; i < 9; i++)
+        node->children[i] = NULL;
+    return node;
+}
+
+void createTreeRecursive(t_node* parent, int depth, int maxDepth, int numChildren) {
+    if (depth > maxDepth) {
+        return;  
+    }
+
+    for (int i = 0; i < numChildren; i++) {
+        t_node* child = createNode(0);
+        parent->children[i] = child;
+        createTreeRecursive(child, depth + 1, maxDepth, numChildren); 
+    }
+}
+
+void createTree(char* move, t_localisation localisation, t_map map) {
+    t_tree tree = createEmptyTree();
+    int costRover = map.costs[localisation.pos.x][localisation.pos.y];
+    t_node *root = createNode(costRover);
+    tree.root = root;
+    int maxDepth = 8;
+    int numChildren = 9;
+
+    createTreeRecursive(tree.root, 1, maxDepth, numChildren);
+    displayTree(&tree);
+}
+
+void displayNode(t_node *node, int depth) {
+    if (node == NULL) return;
+
+    for (int i = 0; i < depth; i++) {
+        printf("  ");
+    }
+    printf("Value: %d\n", node->value);
+
+    for (int i = 0; i < 9; i++) {
+        displayNode(node->children[i], depth + 1);
+    }
+}
+
+void displayTree(t_tree *tree) {
+    if (tree == NULL || tree->root == NULL) {
+        printf("L'arbre est vide.\n");
+        return;
+    }
+    printf("Affichage de l'arbre:\n");
+    displayNode(tree->root, 0);
+}
+
+
+
+
+
+
+
+
 
 int main() {
     t_map map = createMapFromFile("../maps/example1.map");
@@ -22,5 +107,26 @@ int main() {
         printf("\n");
     }
     displayMap(map);
+
+    int Xdep = 3;
+    int Ydep = 3;
+    t_orientation ori = NORTH;
+
+    t_localisation loc = loc_init(Xdep, Ydep, ori);
+
+    printf("x axis: %d, y axis: %d\n", loc.pos.x, loc.pos.y);
+
+
+    t_tree tree = createEmptyTree();
+    t_node *root = createNode(5);
+    tree.root = root;
+
+    root->children[0] = createNode(10);
+    root->children[1] = createNode(15);
+    root->children[0]->children[0] = createNode(20);
+    root->children[0]->children[1] = createNode(25);
+
+    displayTree(&tree);
+
     return 0;
 }
